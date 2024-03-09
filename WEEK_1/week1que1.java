0597e74468e561d38ebc3f3422c284623a5a0c04
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-10 01:29:12 +0530

    repo conflict solved

--- WEEK_1/week1que1.java
+++
@@ -1,8 +0,0 @@
-
-package WEEK_1;
-public class week1que1 {
-    public static void main(String[] args) {
-        System.out.println("Print Hello World.");
-        System.out.print("Hello World and welcome to Java World!");
-        }
-    }
